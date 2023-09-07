14b7269407e2c08e421b75a0ae715fb15c2955dd
yousra chatouane <yousra.ch.etudiante@gmail.com>
2023-09-07 14:55:30 -0700

    /**  * main - Entry point  *  * Return: Always 0 (Success)

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include<stdio.h>
 int main(void)
- {
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+{
 char test[] = "\"Programming is like building a multilingual puzzle";
 puts(test);
 return (0);
