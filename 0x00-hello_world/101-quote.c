f3f9bd6f62fd1530dad6e9fdcc91a3d83fd7aa0d
yousra chatouane <yousra.ch.etudiante@gmail.com>
2023-09-07 15:45:29 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,14 @@
 #include<stdio.h>
+
+/**
+ * main - program to return 1
+ *
+ * Return:Always 1 (success)
+ */
 int main(void)
 {
-write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
-return (0);
+	char err[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	fwrite(&err, sizeof(char), 59, stderr);
+	return (1);
+}
