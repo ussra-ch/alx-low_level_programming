57efab0797d984841dcd0ebeb675e72e84210e08
yousra chatouane <yousra.ch.etudiante@gmail.com>
2023-09-07 15:43:10 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+int main(void)
+{
+write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
+return (0);
