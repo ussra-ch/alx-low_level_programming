624661e02657ae784e93e990a3a113e8fb2c29d0
yousra chatouane <yousra.ch.etudiante@gmail.com>
2023-09-07 15:18:39 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * Description:
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf(Size of a char : % lu byte(s)\n", (unsigned long)sizeof (char));
+	printf(Size of a an int : % lu byte(s) \n", (unsigned long)sizeof(int));
+	printf(Size of a long : % lu bytes(s) \n", (unsigned long)sizeof (long));
+	printf(Size of a long long int : % lu byte(s) \n", (unsigned long)sizeof(d));
+	printf(size of a float : % i byte(s) \n", (unsigned long)sizeof (%f));
+	return (0);
+}
