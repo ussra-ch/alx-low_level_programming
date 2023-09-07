54a12d83193a36689b5c193402ea4ad2b9bc37d8
yousra chatouane <yousra.ch.etudiante@gmail.com>
2023-09-07 14:43:59 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 int main(){
-char test[]= "Programming is like building a multilingual puzzle";
+char test[]= "\"Programming is like building a multilingual puzzle";
 puts(test);
 return 0;
 }
