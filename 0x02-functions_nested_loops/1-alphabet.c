f11afa0402de7459ea1f97e67ec2c85c51022d07
yousra chatouane <yousra.ch.etudiante@gmail.com>
2023-09-12 13:55:05 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -9,7 +9,7 @@ void print_alphabet(void)
 	char i = 'a';
         while (i <= 'z')
         {
-                putchar('&i');
+                putchar('i');
                 i++;
         }
 	return;
