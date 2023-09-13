8988000ff6d97ee9aa850b0edb1274bab46ac20d
yousra chatouane <yousra.ch.etudiante@gmail.com>
2023-09-13 05:29:57 -0700

    1-alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,23 @@
 #include "main.h"
 
 /**
- * print_alphabet - description
+ * print_alphabet - Prints the lowercase alphabet followed by a newline
+ *
+ * Description: This function prints the lowercase alphabet in a
+ *              sequential order followed by a newline character.
+ *
  * Return: void (Success)
  */
 void print_alphabet(void)
 {
-	char i = 'a';
-        while (i <= 'z')
-        {
-                _putchar(i);
-                i++;
-        }
-	return;
+    char i = 'a';
+
+    while (i <= 'z')
+    {
+        _putchar(i);
+        i++;
+    }
+
+    _putchar('\n');
 }
 
--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -1,12 +1,17 @@
-#include<stdio.h>
+#include <stdio.h>
 #include "main.h"
 
 /**
- * main - description
+ * main - Entry point of the program
+ *
+ * Description: This is the main function of the program, and it calls
+ * the print_alphabet function to print the lowercase alphabet.
+ *
  * Return: 0 (Success)
  */
-int main (void)
+int main(void)
 {
-        print_alphabet();
-        return (0);
+    print_alphabet();
+    return (0);
 }
+
