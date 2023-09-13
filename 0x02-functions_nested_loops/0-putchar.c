1fc7e9a119908ebcecbfe703c673e0d684b8c75e
yousra chatouane <yousra.ch.etudiante@gmail.com>
2023-09-13 03:30:51 -0700

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,5 @@
 #include "main.h"
-#include<unistd.h>
-
-/**
- *  _putchar - short describtion
- *  @c: Description of parameter x
- *  Return: 1 (Success)
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+#inclde "My_fun.c"
 /**
  * main- print -putchar
  * _putchar - short words
---
+++ 0x02-functions_nested_loops/my_fun.c
@@ -0,0 +1,9 @@
+/**
+ *  _putchar - short describtion
+ *  @c: Description of parameter x
+ *  Return: 1 (Success)
+ */
+int _putchar(char c)
+{
+        return (write(1, &c, 1));
+}
