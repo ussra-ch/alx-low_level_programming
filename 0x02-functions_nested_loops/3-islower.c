e707aa42685955a9b0ab2f5695d1ed718e45bbc1
yousra chatouane <yousra.ch.etudiante@gmail.com>
2023-09-13 07:12:49 -0700

    3-islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,21 +1,21 @@
 #include "main.h"
 /**
- * _islower - checks the lowercase alphabet 
+ * _islower - checks the lowercase alphabet
  *
  * Description: This function prints the lowercase alphabet in a
  * sequential order followed by a newline character.
- *
+ *@c: normal int
  * Return: Always (Success)
  */
 
 int _islower(int c)
 {
-	if ( c >= 'a' && c<= 'z')
+	if (c >= 'a' && c <= 'z')
 	{
-		return(1);
+		return (1);
 	}
 	else
 	{
-		return(0);
+		return (0);
 	}
 }
