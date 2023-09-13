e549b7a75d8ca6fab78a341d4a95dc49d2c15349
yousra chatouane <yousra.ch.etudiante@gmail.com>
2023-09-13 06:40:05 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Prints the lowercase alphabet 10 times followed by a newline
+ *
+ * Description: This function prints the lowercase alphabet in a
+ * sequential order followed by a newline character.
+ *
+ * Return: void (Success)
+ */
+
+void print_alphabet_x10(void)
+{
+	int j;
+	for (j = 0; j<10; j++)
+	{
+		char i;
+		for(i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+	return ;
+}
