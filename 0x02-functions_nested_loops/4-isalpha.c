b9ea3b5e5b3c033e81f5d8976c31012f94323d5d
yousra chatouane <yousra.ch.etudiante@gmail.com>
2023-09-13 07:31:27 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * int _isalpha - Prints the lowercase alphabet followed by a newline
+ * _isalpha - Prints the lowercase alphabet followed by a newline
  *
  * Description: This function prints the lowercase alphabet in a
  * sequential order followed by a newline character.
@@ -9,7 +9,7 @@
  */
 int _isalpha(int c)
 {
-	if ((c >= 'a' && c <= 'z') ||(c >= 'A' && c <= 'Z'))
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 	{
 		return (1);
 	}
