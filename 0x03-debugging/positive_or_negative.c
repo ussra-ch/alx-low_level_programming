855854ad19a417e3ee1d178487e118057a2ac011
yousra chatouane <yousra.ch.etudiante@gmail.com>
2023-09-14 06:40:48 -0700

    task 0

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,26 @@
+
+/**
+ * positive_or_negative - the function
+ * Decription: short one
+ * @n: integer
+ * Return: Always true
+ */
+#include<stdio.h>
+void positive_or_negative(int n)
+{
+	if (n < 0)
+	{
+		printf("%d is negative", n);
+		printf("\n");
+	}
+	else if (n == 0)
+	{
+		printf("0 is zero");
+		printf("\n");
+	}
+	else
+	{
+		printf("%d is positive", n);
+		printf("\n");
+	}
+}
