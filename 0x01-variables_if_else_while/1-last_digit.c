fe18050851b8b56e0b2309ae2a4939d3e25223f3
yousra chatouane <yousra.ch.etudiante@gmail.com>
2023-09-14 03:42:35 -0700

    tsk 0

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - test
+ * Description: the project's description
+ * Return: 0 (Success)
+ */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+	char[]=:wq
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (
+	return (0);
+}
