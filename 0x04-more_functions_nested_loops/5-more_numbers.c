98be5c42485358947c1ee79e4d306aa0f973114b
yousra chatouane <yousra.ch.etudiante@gmail.com>
2023-09-15 09:19:25 -0700

    task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * more_numbers - the function
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			_putchar(j + '0');
+		}
+		for (j = 10; j <= 14; j++)
+		{
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
+
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -4,3 +4,4 @@
 	void print_numbers(void);
 	int _putchar(char);
 	void print_most_numbers(void);
+	void more_numbers(void);
