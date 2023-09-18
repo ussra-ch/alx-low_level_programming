60356bc0f895f00caba0b7d485aa20ea96df810c
yousra chatouane <yousra.ch.etudiante@gmail.com>
2023-09-18 12:02:18 -0700

    task7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ * puts_half - the function
+ * @str: the input
+ */
+void puts_half(char *str)
+{
+	int i = 0, len, j;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	len = i;
+
+	if (len % 2 == 0)
+	{
+		for (j = len/2; j < len; j++)
+		{
+			printf("%c", str[j]);
+		}
+	}
+	else
+	{
+
+		for (j = (len - 1) / 2; j < len; j++)
+		{
+				printf("%c", str[j]);
+		}
+	}
+	printf("\n");
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -6,3 +6,5 @@ void reset_to_98(int *n);
  void print_rev(char *s);
  void rev_string(char *s);
  void puts2(char *str);
+ void puts_half(char *str);
+
