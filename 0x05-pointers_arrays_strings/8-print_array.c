353234aa02e1c7dae3a38ccd1744293086b9020d
yousra chatouane <yousra.ch.etudiante@gmail.com>
2023-09-18 14:24:42 -0700

    task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include"main.h"
+/**
+ * print_array - the funtion
+ * @a: the first pointer of the array
+ * @n: the second input
+ */
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	for (i = 0; i < n - 1; i++)
+	{
+		printf("%d, ", a[i]);
+	}
+	printf("%d", a[n - 1]);
+	printf("\n");
+}
