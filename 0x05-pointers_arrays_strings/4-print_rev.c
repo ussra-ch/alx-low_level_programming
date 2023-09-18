70fb61c224c84bc336298448944d77d0f8bebc45
yousra chatouane <yousra.ch.etudiante@gmail.com>
2023-09-18 08:27:28 -0700

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_rev - the function
+ * @s: the input
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = '\0'; i < 0; i--)
+	{
+		_putchar(*s);
+	}
+}
+
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,3 +3,4 @@ void reset_to_98(int *n);
  int _strlen(char *s);
  void _puts(char *str);
  int _putchar(char c);
+ void print_rev(char *s);
