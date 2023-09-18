a773eb96e71f4e205be8b1935c0099ec406101d1
yousra chatouane <yousra.ch.etudiante@gmail.com>
2023-09-18 11:37:50 -0700

    task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,17 +1,19 @@
 #include "main.h"
-#include<stdio.h>
 /**
  * puts2 - the function
  * @str: the input
  */
 void puts2(char *str)
 {
-	int i = 0;
+	int i, len = 0;
 
-	while (str[i] + 1 != '\0')
+	while (str[len] != '\0')
 	{
-		printf("%c", str[i]);
-		i += 2;
+		len++
 	}
-	printf("\n");
+	for (i = 0; i < len; i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar("\n");
 }
