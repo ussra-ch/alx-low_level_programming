decd27baaf7bfeb62df3ebd001e34447413cbe2d
yousra chatouane <yousra.ch.etudiante@gmail.com>
2023-09-20 06:32:35 -0700

    task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strcat - the function
+ * @dest: the first input
+ * @src: the second one
+ * Return: pointer to char (Success)
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0, j;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	for (j = 0;src[j] != '\0'; j++)
+	{
+		dest[i] = src[j];
+		i++;
+	}
+	return (dest);
+}
+
