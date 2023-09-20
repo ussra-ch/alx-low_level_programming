974cc5cfaf2900d54db8ccf0a90b3c2a44943d48
yousra chatouane <yousra.ch.etudiante@gmail.com>
2023-09-20 13:49:28 -0700

    task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * lett - the function
+ * @a: the input
+ * Return: pointer (Success)
+ */
+char *leet(char *str)
+{
+	char leetMap[128] = {0};
+
+	int i = 0;
+
+	leetMap['a'] = leetMap['A'] = '4';
+	leetMap['e'] = leetMap['E'] = '3';
+	leetMap['o'] = leetMap['O'] = '0';
+	leetMap['t'] = leetMap['T'] = '7';
+	leetMap['l'] = leetMap['L'] = '1';
+
+	while (*str)
+	{
+		if (leetMap[(int)*str])
+		{
+			str[i] = leetMap[(int)*str];
+		}
+		str++;
+	}
+	return (str);
+}
