a1044f850eeb3132edb491bcbd2bbf8839cbe65d
yousra chatouane <yousra.ch.etudiante@gmail.com>
2023-09-20 11:57:02 -0700

    task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,17 +8,18 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i = 0, j;
+	int i = 0, j = 0;
 
 	while (dest[i] != '\0')
 	{
 		i++;
 	}
-	for (j = 0; j < n; j++)
+	for (j = 0; j < n && src[j] != '\0'; j++)
 	{
 		dest[i] = src[j];
 		i++;
 	}
+	dest[i] = '\0';
 
 	return (dest);
 }
