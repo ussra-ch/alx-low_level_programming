08273a846dbb0935302115205d12cb3c5dac022d
yousra chatouane <yousra.ch.etudiante@gmail.com>
2023-09-21 02:38:25 -0700

    task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * cap_string - the function
+ * @a: the input
+ * Return: pointer to a char (Success)
+ */
+char *cap_string(char *a)
+{
+	int i = 0;
+
+	if (a[0]  >= 'a' && a[0] <= 'z')
+	{
+		a[0] = a[0] - 32;
+	}
+	while (a[i] != '\0')
+	{
+		if (a[i] == ',' || a[i] == ';' || a[i] == '.' || a[i] == ' ' || a[i] == '!' || a[i] == '?' || a[i] == '"' || a[i] == ',' || a[i] == '(' || a[i] == ')' || a[i] == ',' || a[i] == '{' || a[i] == '}')
+		{
+			if (a[i + 1] >= 'a' && a[i + 1] <= 'z')
+			{
+				a[i + 1] = a[i + 1] - 32;
+			}
+		}
+		i++;
+	}
+	return (a);
+}
