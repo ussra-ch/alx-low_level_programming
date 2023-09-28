7db347ff5b2e83bd9549b0e01ed9fdbafee10b8c
yousra chatouane <yousra.ch.etudiante@gmail.com>
2023-09-28 11:12:00 -0700

    task 3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * factorial - the function
+ * @n: input
+ * Return: int (Success)
+ */
+int factorial(int n)
+{
+	if (n == 0)
+	{
+		return (1);
+	}
+	return (n * factorial(n - 1));
+}
