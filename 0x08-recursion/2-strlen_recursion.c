7a7fd09ece5cdbda67a4853157431e52bcf5fabb
yousra chatouane <yousra.ch.etudiante@gmail.com>
2023-09-28 11:04:22 -0700

    task 3

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,13 +6,9 @@
  */
 int _strlen_recursion(char *s)
 {
-	int len = 0;
-	
-	while (*s != '\0')
+	if (*s == '\0')
 	{
-		len++;
-		_strlen_recursion(s + 1);
-		s++;
+		return (0);
 	}
-	return (len);
+	return (1 + _strlen_recursion(s + 1));
 }
