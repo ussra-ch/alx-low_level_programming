7bfee47c1cd4a587505a021429d808e7813bf69f
yousra chatouane <yousra.ch.etudiante@gmail.com>
2023-10-03 07:59:49 -0700

    task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,7 +12,7 @@ char *create_array(unsigned int size, char c)
 
 	unsigned int i = 0;
 
-	if (size == 0 || ar == NULL)
+	if (size == 0)
 	{
 		return (NULL);
 	}
@@ -25,5 +25,9 @@ char *create_array(unsigned int size, char c)
 			i++;
 		}
 		return (ar);
+	}
+	else if (ar == NULL)
+	{
+		return (NULL);
 	}
 }
