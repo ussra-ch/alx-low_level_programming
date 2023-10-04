129ecb08f06e13124a74251897832b9be97b3c58
yousra chatouane <yousra.ch.etudiante@gmail.com>
2023-10-04 12:00:15 -0700

    task 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -13,26 +13,25 @@ int **alloc_grid(int width, int height)
 	int **arr;
 
 	arr = (int **)malloc(sizeof(int *) * height);
-
-	if (width <= 0 || height <= 0)
+	if (arr != NULL)
 	{
-		return (NULL);
-	}
-	for (i = 0; i < height; i++)
-	{
-		arr[i] = (int *)malloc(sizeof(int *) * width);
-	}
-	for (i = 0; i < height; i++)
-	{
-		for (j = 0; j < width; j++)
-			arr[i][j] = 0;
-	}
-	if (arr == NULL)
-	{
-		return (NULL);
+		if (width <= 0 || height <= 0)
+		{
+			return (NULL);
+		}
+		for (i = 0; i < height; i++)
+		{
+			arr[i] = (int *)malloc(sizeof(int *) * width);
+		}
+		for (i = 0; i < height; i++)
+		{
+			for (j = 0; j < width; j++)
+				arr[i][j] = 0;
+		}
+		return (arr);
 	}
 	else
 	{
-		return (arr);
+		return (NULL);
 	}
 }
