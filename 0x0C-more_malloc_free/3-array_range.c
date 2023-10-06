c02f58b5f33066c5bd689780d958e5072c18b8f4
yousra chatouane <yousra.ch.etudiante@gmail.com>
2023-10-06 16:54:09 -0700

    task 4

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -17,7 +17,7 @@ int *array_range(int min, int max)
 		return (NULL);
 	}
 
-	ar = malloc(sizeof(int) * (max - min + 2));
+	ar = malloc(sizeof(int) * (max - min + 1));
 
 	for (j = 0, i = min; j < (max - min + 1) && i <= max; j++, i++)
 	{
