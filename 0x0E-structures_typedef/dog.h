6f3fae61f6d4d84b0d0df2ef61839d24b3e68a08
yousra chatouane <yousra.ch.etudiante@gmail.com>
2023-10-10 08:19:36 -0700

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - short structure
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: awner of the dog
+ *
+ * Description: structure containing some information about the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
