8cae9184603882331e9e62563df24048c9840de9
yousra chatouane <yousra.ch.etudiante@gmail.com>
2023-10-10 13:26:25 -0700

    task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -16,6 +16,15 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	new__dog = malloc(sizeof(dog_t));
 
+	if (new__dog == NULL)
+	{
+		return (NULL);
+	}
+	if (new__dog->name == NULL || new__dog->owner == NULL)
+	{
+		free(new__dog);
+		return (NULL);
+	}
 	new__dog->name = malloc(sizeof(name) + 1);
 
 	for (i = 0; name[i]; i++)
@@ -23,10 +32,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		new__dog->name[i] = name[i];
 	}
 	new__dog->name[i] = '\0';
-
 	new__dog->owner = malloc(sizeof(owner) + 1);
-
-	i = 0;
 
 	for (i = 0; owner[i]; i++)
 	{
@@ -35,9 +41,5 @@ dog_t *new_dog(char *name, float age, char *owner)
 	new__dog->owner[i] = '\0';
 	new__dog->age = age;
 
-	if (new__dog == NULL)
-	{
-		return (NULL);
-	}
 	return (new__dog);
 }
