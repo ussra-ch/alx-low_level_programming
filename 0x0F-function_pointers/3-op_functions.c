927d40f537170fc21a54acc1788d1dec5d12537b
yousra chatouane <yousra.ch.etudiante@gmail.com>
2023-10-11 13:53:01 -0700

    task 3

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+/**
+ * op_add - adds two inputs
+ * @a: first input
+ * @b: second input
+ * Return: sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - substracte two inputs
+ * @a: first input
+ * @b: second input
+ * Return: sub
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multiplies two inputs
+ * @a: first input
+ * @b: second input
+ * Return: mul
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - adds two inputs
+ * @a: first input
+ * @b: second input
+ * Return: div
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - adds two inputs
+ * @a: first input
+ * @b: second input
+ * Return: mod
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
