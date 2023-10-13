3b483e4712b6ee22340d8823d00354e6a3a7582c
yousra chatouane <yousra.ch.etudiante@gmail.com>
2023-10-13 15:49:50 -0700

    task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _memset - function that fills memory with a constant byte
+ * @s: the memory area pointed
+ * @b: constant byte
+ * @n: bytes
+ * Return: ptr to he memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	while (n)
+	{
+		s[n] = b;
+		n--;
+	}
+	return (s);
+}
