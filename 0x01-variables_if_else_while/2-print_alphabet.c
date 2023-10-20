652732854f194928055f369b63bc5343a225a856
yousra chatouane <yousra.ch.etudiantw@gmail.com>
2023-10-20 09:40:32 -0700

    task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,11 +5,12 @@
  */
 int main(void)
 {
-	char al[25] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i','k', 'l', 'm', 'n',
-		'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+	char al[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+		'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 
 	int i;
-	for (i = 0; i < 25; i++)
+
+	for (i = 0; i < 26; i++)
 	{
 		putchar(al[i]);
 	}
