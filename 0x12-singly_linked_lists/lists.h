327b485b0122919c500777db3d3ba5ffc000a758
yousra chatouane <yousra.ch.etudiantw@gmail.com>
2023-10-20 03:46:50 -0700

    task 0

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,11 +1,18 @@
-#ifndef MY_FUN
-#define MY_FUN
+#ifndef LISTS
+#define LISTS
 
 #include <stddef.h>
+/**
+ * struct list_s - singly linked list
+ * @str: string
+ * @len: integer (length)
+ * @next: ptr to the next node of the list
+ *
+ * Description: simply linked list.
 typedef struct list_s
 {
 	char *str;
-	
+
 	int len;
 
 	struct list_s *next;
