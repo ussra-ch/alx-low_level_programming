973bb6f70a86afad16c89c77a357757fe3e6e42a
yousra chatouane <yousra.ch.etudiantw@gmail.com>
2023-10-20 09:17:51 -0700

    task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,12 +10,12 @@
 /* betty style doc for function main goes there */
 int main(void)
 {
-	int n;
+	int n, last;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	int last = n;
+	last = n;
 	while (last)
 	{
 		last = last % 10;
