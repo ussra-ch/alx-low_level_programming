6263caadff93242a5eeb72473abf1895dd529e21
yousra chatouane <yousra.ch.etudiantw@gmail.com>
2023-10-20 14:06:22 -0700

    task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,17 +1,27 @@
 #include "main.h"
 /**
+ * mine - claculates the sqrt of a num
+ * @n: input
+ * @a: input
+ * Return: ineger
+ */
+int mine(int n, int a)
+{
+	if (n == (a * a))
+		return (a);
+	if (n <= (a * a))
+		return (-1);
+	else
+		return (mine(n, a + 1));
+}
+/**
  * _sqrt_recursion - returns the natural square root of a number
  * @n: input
  * Return: natural square root of a number
  */
 int _sqrt_recursion(int n)
 {
-	int i = 0;
-
-	if (n == (i * i))
-		return (i);
-	else if (n != i * i)
+	if (n <= 0)
 		return (-1);
-	_sqrt_recursion(i + 1);
-	return (n);
+	return (mine(n, 0));
 }
