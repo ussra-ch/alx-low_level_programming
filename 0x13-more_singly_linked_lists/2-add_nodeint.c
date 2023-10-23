75dc012c29176d783df67c698792cc68df72a0f4
yousra chatouane <yousra.ch.etudiantw@gmail.com>
2023-10-23 10:58:04 -0700

    task 2

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 /**
  * add_nodeint - adds a new node at the beginning of a listint_t list
- * @head: ptr to the first node of the linked list
+ * @*head: ptr to the first node of the linked list
  * @n: int
  * Return: the address of the new element, or NULL if it failed
  */
