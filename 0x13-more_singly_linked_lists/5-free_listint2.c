f5c5c03970cb5d9516bb29dd4da7202f6232b9af
yousra chatouane <yousra.ch.etudiantw@gmail.com>
2023-10-23 11:40:50 -0700

    taks 6

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 #include <stdlib.h>
 /**
- * free_listint2 -  frees a listint_t list
+ * free_listint2 - frees a listint_t list
  * @head: the head of the linked list
  */
 void free_listint2(listint_t **head)
