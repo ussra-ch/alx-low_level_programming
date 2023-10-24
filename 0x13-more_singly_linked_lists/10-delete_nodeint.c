f6e57c6c36f486a77031c9b9c938c44d3ece2639
yousra chatouane <yousra.ch.etudiantw@gmail.com>
2023-10-24 11:49:30 -0700

    taks 10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -15,7 +15,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	tmp = malloc(sizeof(listint_t));
 	t = malloc(sizeof(listint_t));
-	if (tmp == NULL)
+	if (tmp == NULL || head == NULL)
 		return (-1);
 	tmp = *head;
 	if (index == 0)
