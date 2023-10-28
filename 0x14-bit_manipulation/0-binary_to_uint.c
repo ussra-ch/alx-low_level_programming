a19c3bf1573a60fa533b87ebbcb84c8e9caf6962
yousra chatouane <yousra.ch.etudiantw@gmail.com>
2023-10-28 12:57:24 -0700

    task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-* int binary_to_uint - converts a binary number to an unsigned int
+* binary_to_uint - converts a binary number to an unsigned int
 * @b: pointer to 0 and 1 char
 * Return: the converted number, or 0 if
 * there is one or more chars in the string b that is not 0 or 1,b is NULL
