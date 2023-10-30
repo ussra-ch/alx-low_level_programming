cf62f5cf0b0cc574d35000ddc4d3abf40f342a60
yousra chatouane <yousra.ch.etudiantw@gmail.com>
2023-10-30 12:39:57 -0700

    task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * create_file - function that creates a file
+ * @filename: the file's created name
+ * @text_content: the content of the file
+ * Return: 1 on success, -1 on failure (file can not be created,file can not be written,write “fails”,etc
+ * if filename is NULL return -1
+ * if text_content is NULL create an empty file
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int file;
+
+	if (filename == NULL)
+		return (-1);
+	if (text_content == NULL)
+		text_content = "";
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (file == -1)
+		return (-1);
+	return (1);
+}
