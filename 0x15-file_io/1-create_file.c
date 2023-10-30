a366e6e110320ee1751407ec3df8e1abbcfc08cb
yousra chatouane <yousra.ch.etudiantw@gmail.com>
2023-10-30 12:51:02 -0700

    taks 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,7 +3,7 @@
  * create_file - function that creates a file
  * @filename: the file's created name
  * @text_content: the content of the file
- * Return: 1 on success, -1 on failure (file can not be 
+ * Return: 1 on success, -1 on failure (file can not be
  * created,file can not be written,write “fails”,etc
  * if filename is NULL return -1
  * if text_content is NULL create an empty file
@@ -16,8 +16,11 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content == NULL)
 		text_content = "";
+	while (text_content != '\0')
+		i++;
 	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (file == -1)
 		return (-1);
+	write(file, text_content, i);
 	return (1);
 }
