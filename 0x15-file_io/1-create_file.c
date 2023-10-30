7a144b1ec0df7d27491520b645c298e900326c29
yousra chatouane <yousra.ch.etudiantw@gmail.com>
2023-10-30 12:52:38 -0700

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,9 @@ int create_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 		text_content = "";
 	while (text_content != '\0')
+	{
 		i++;
+	}
 	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (file == -1)
 		return (-1);
