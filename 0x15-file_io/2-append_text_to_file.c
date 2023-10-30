b5732f83743952640816ffa4c5622cb89c376962
yousra chatouane <yousra.ch.etudiantw@gmail.com>
2023-10-30 13:16:29 -0700

    task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -2,10 +2,8 @@
 /**
  * append_text_to_file - function that appends text at the end of a file
  * @filename: the file name
- * @text_contennt: the content of the file
+ * @text_content: the content of the file
  * Return: 1 on success and -1 on failure
- * If filename is NULL return -1
- * Return 1 if the file exists and -1 if the file does not exist
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
