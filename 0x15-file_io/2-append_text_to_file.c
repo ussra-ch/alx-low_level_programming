bb174f18748753b585f8aa361c6cfa525d664c47
yousra chatouane <yousra.ch.etudiantw@gmail.com>
2023-10-30 13:07:42 -0700

    task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -17,7 +17,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		text_content = "";
 	while (text_content[i] != '\0')
 		i++;
-	filr = open(filename,  O_WRONLY | O_APPEND);
+	file = open(filename,  O_WRONLY | O_APPEND);
 	if (file == -1)
 		return (-1);
 	write(file, text_content, i);
