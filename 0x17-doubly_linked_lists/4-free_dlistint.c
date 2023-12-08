b32617610bc5201af08e48f4d8c926c9cbaf8fbf
zakaria chatouane <yousra.ch.etudiante@gmail.com --global user.name yousra>
2023-12-08 01:15:03 +0100

    error fixed

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -5,13 +5,14 @@
 */
 void free_dlistint(dlistint_t *head)
 {
-    dlistint_t *tmp, *current;
-    tmp = head->next;
-    while (tmp)
-    {
-        current = tmp->next;
-        free(tmp);
-        tmp = current;
-    }
-    free(current);
+	dlistint_t *tmp, *current;
+
+	tmp = head->next;
+	while (tmp)
+	{
+		current = tmp->next;
+		free(tmp);
+		tmp = current;
+	}
+	free(current);
 }
